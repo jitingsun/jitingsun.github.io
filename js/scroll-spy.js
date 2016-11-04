c6b60957009ea2d345de25dd64d531bdd331964d
jitingsun <846523689@qq.com>
2016-11-04 17:02:50 +0800

    push

--- js/scroll-spy.js
+++ js/scroll-spy.js
@@ -82,14 +82,14 @@ function getItems(ary) {
 function spy(items, className) {
   var find = 0;
 
-  for (var i = 0, l = items.length-1; i < l; i++) {
+  for (var i = 0, l = items.length; i < l; i++) {
     if ($body.scrollTop < items[i].top - items[i].height / 3) {
       find = i;
       break;
     }
   }
 
-  for (var j = 0, _l = items.length-1; j < _l; j++) {
+  for (var j = 0, _l = items.length; j < _l; j++) {
     util.removeClass(items[j].elem, className);
   }
 
